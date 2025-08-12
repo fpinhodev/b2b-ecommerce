ad889e4d17d5de1ccf92006a3c2a8da66ae7c83a
Filipe Pinho <fpinhodev@gmail.com>
2025-08-12 12:40:51 +0100

    storybook playground added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
@@ -9,40 +12,37 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    rules: {
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-empty-object-type': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-      // Consistently import navigation APIs from `@/i18n/routing`
-      'no-restricted-imports': [
-        process.env.NODE_ENV === 'production' ? 'off' : 'warn',
-        {
-          name: 'next/link',
-          message: 'Please import from `@/i18n/routing` instead.',
-        },
-        {
-          name: 'next/navigation',
-          importNames: ['redirect', 'permanentRedirect', 'useRouter', 'usePathname'],
-          message: 'Please import from `@/i18n/routing` instead.',
-        },
-      ],
-      // Avoid hardcoded labels
-      'react/jsx-no-literals': process.env.NODE_ENV === 'production' ? 'off' : 'warn',
-    },
+const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript'), {
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-empty-object-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    // Consistently import navigation APIs from `@/i18n/routing`
+    'no-restricted-imports': [
+      process.env.NODE_ENV === 'production' ? 'off' : 'warn',
+      {
+        name: 'next/link',
+        message: 'Please import from `@/i18n/routing` instead.',
+      },
+      {
+        name: 'next/navigation',
+        importNames: ['redirect', 'permanentRedirect', 'useRouter', 'usePathname'],
+        message: 'Please import from `@/i18n/routing` instead.',
+      },
+    ],
+    // Avoid hardcoded labels
+    'react/jsx-no-literals': process.env.NODE_ENV === 'production' ? 'off' : 'warn',
   },
-]
+}, ...storybook.configs["flat/recommended"]]
 
 export default eslintConfig
